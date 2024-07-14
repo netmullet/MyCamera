bf559179dc5c4a5c85b95f816091d78a95a74ec1
netmullet <lipper.corp@gmail.com>
2024-07-14 09:47:40 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Otsuka on 2024/07/14.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
